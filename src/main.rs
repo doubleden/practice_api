54b4609c5114a3f6bfbc25dc0db5ba4566eb95a2
Denis Denisov <denis.denisov8701@gmail.com>
2024-06-16 02:20:26 +0200

    changing

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use serde::{Serialize, Deserialize};
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_files as afs;
 use uuid::Uuid;
-const IP: &str = "127.0.0.1:8080";
-
+const IP: &str = "0.0.0.0:8080";
+const IMAGE_IP: &str = "192.168.18.234:8080";
 #[derive(Serialize, Deserialize)]
 struct Exercise {
     id: Uuid,
@@ -33,7 +33,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Жим ног",
             "Упражнение для прокачки ног, которое выполняется в специальном тренажёре, горизонтальном или вертикальном (наклонном)",
-            &format!("http://{IP}/images/leg_press.png")
+            &format!("http://{IMAGE_IP}/images/leg_press.png")
         )
     );
 
@@ -42,7 +42,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Присяд со штангой",
             "Положите штангу на плечи, выставьте одну ногу вперед, вторую отведите назад. На вдохе опустите таз до параллели бедра с полом. На выдохе вернитесь в исходное положение. Упражнение эффективно нагружает мышцы бедер и ягодиц.",
-            &format!("http://{IP}/images/squat_with_barbell.jpg")
+            &format!("http://{IMAGE_IP}/images/squat_with_barbell.jpg")
         )
     );
 
@@ -51,7 +51,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Присяд с гантелей",
             "Альтернатива упражнения - присяд со штангой",
-            &format!("http://{IP}/images/squat_with_dumbbells.jpeg")
+            &format!("http://{IMAGE_IP}/images/squat_with_dumbbells.jpeg")
         )
     );
 
@@ -60,7 +60,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Выпады назад",
             "Упражнение выпадов назад 一 это упражнение, которое направлено на развитие мышц ног, таких как ягодицы, бедра и икры.",
-            &format!("http://{IP}/images/back_lunges.jpg.webp")
+            &format!("http://{IMAGE_IP}/images/back_lunges.jpg.webp")
         )
     );
 
@@ -69,7 +69,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Болгарский выпад",
             "упражнение для тренировки нижней части тела, особенно ягодичных и бедренных мышц.",
-            &format!("http://{IP}/images/lunges_with_bench.jpg")
+            &format!("http://{IMAGE_IP}/images/lunges_with_bench.jpg")
         )
     );
 
@@ -78,7 +78,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Выпады вперед",
             "Классика",
-            &format!("http://{IP}/images/lunge_steps.jpeg.webp")
+            &format!("http://{IMAGE_IP}/images/lunge_steps.jpeg.webp")
         )
     );
 
@@ -87,7 +87,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Сгибание ног",
             "Сгибание ног в тренажёре — упражнение для развития силы и наращивания объёма мышц на задней стороне бедра.",
-            &format!("http://{IP}/images/leg_curl.jpg")
+            &format!("http://{IMAGE_IP}/images/leg_curl.jpg")
         )
     );
 
@@ -96,7 +96,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Разгибания ног",
             "Разгибания ног в тренажёре — упражнение для развития силы и наращивания объёма мышц на передней стороне бедра.",
-            &format!("http://{IP}/images/leg_extensions.jpg")
+            &format!("http://{IMAGE_IP}/images/leg_extensions.jpg")
         )
     );
 
@@ -105,7 +105,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Тяга классика",
             "Большая часть нагрузки приходится на спину — разгибатели и трапециевидные мышцы.",
-            &format!("http://{IP}/images/traction_classic.jpeg")
+            &format!("http://{IMAGE_IP}/images/traction_classic.jpeg")
         )
     );
 
@@ -114,7 +114,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Тяга сумо",
             "Приседания сумо — это комплексное упражнение, при выполнении которого работает много мышц. Основные мышцы: квадрицепсы, ягодичные мышцы, бицепс бедра, икры и внутренняя поверхность бедер, и другие, более мелкие, внутренние мышцы бедра.",
-            &format!("http://{IP}/images/sumo_deadlift.jpg")
+            &format!("http://{IMAGE_IP}/images/sumo_deadlift.jpg")
         )
     );
 
@@ -123,7 +123,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Пистолетик",
             "«Пистолетик» — приседание на одной ноге без опоры, относится к силовым упражнениям. Его биомеханика аналогична обычным приседаниям. Но выполнять «пистолетик» значительно тяжелее, так как рабочая нога получает непривычную нагрузку.",
-            &format!("http://{IP}/images/gun_squat.jpg")
+            &format!("http://{IMAGE_IP}/images/gun_squat.jpg")
         )
     );
 
@@ -132,7 +132,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Броски гири",
             "Броски гири, так или иначе, приводит в тонус все мышцы, но в большей степени участвуют мышцы бедра, ягодиц, спины и живота",
-            &format!("http://{IP}/images/throws_weights.jpeg")
+            &format!("http://{IMAGE_IP}/images/throws_weights.jpeg")
         )
     );
 
@@ -141,7 +141,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Махи лёжа на боку",
             "Такие махи направлены на укрепление внутренней поверхности бедра и нижней части брюшной мускулатуры.",
-            &format!("http://{IP}/images/swing_lying.png")
+            &format!("http://{IMAGE_IP}/images/swing_lying.png")
         )
     );
 
@@ -150,7 +150,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Ягодичный мостик",
             "Ягодичный мостик – это наилучшее упражнения для проработки ягодичной мышцы.",
-            &format!("http://{IP}/images/gluteal_bridge.jpg")
+            &format!("http://{IMAGE_IP}/images/gluteal_bridge.jpg")
         )
     );
 
@@ -159,7 +159,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Свидение ног",
             "Сведение ног – востребованное физическое упражнение для укрепления нижнего пояса.",
-            &format!("http://{IP}/images/date_legs.jpg")
+            &format!("http://{IMAGE_IP}/images/date_legs.jpg")
         )
     );
 
@@ -168,7 +168,7 @@ async fn get_exercises() -> impl Responder {
             "legs",
             "Развидение ног",
             "Разведение ног - позволяет тренировать внешние (отводящие) мышцы бёдер и ягодичные мышцы.",
-            &format!("http://{IP}/images/unraveling_legs.jpg")
+            &format!("http://{IMAGE_IP}/images/unraveling_legs.jpg")
         )
     );
     // LEGS END
@@ -177,7 +177,7 @@ async fn get_exercises() -> impl Responder {
             "chest",
             "Жим лёжа",
             "Жим лёжа — базовое физическое упражнение со свободным весом.",
-            &format!("http://{IP}/images/bench_press.jpg.webp")
+            &format!("http://{IMAGE_IP}/images/bench_press.jpg.webp")
         )
     );
 
@@ -186,7 +186,7 @@ async fn get_exercises() -> impl Responder {
             "chest",
             "Жим лёжа <30",
             "Смена угла наклона жима позволяет уделять внимание конкретной части груди, что способствует построению нужного рельефа",
-            &format!("http://{IP}/images/bench_press30.jpg.jpeg")
+            &format!("http://{IMAGE_IP}/images/bench_press30.jpg.jpeg")
         )
     );
 
@@ -195,7 +195,7 @@ async fn get_exercises() -> impl Responder {
             "chest",
             "Жим лёжа гантелями",
             "Жим гантелей обеспечивает более широкий диапазон движения, так как каждая рука двигается независимо.",
-            &format!("http://{IP}/images/bench_dmb.png")
+            &format!("http://{IMAGE_IP}/images/bench_dmb.png")
         )
     );
 
@@ -204,7 +204,7 @@ async fn get_exercises() -> impl Responder {
             "chest",
             "Жим лёжа гантелями <30",
             "Жим гантелей обеспечивает более широкий диапазон движения, так как каждая рука двигается независимо. Нагрузка идет на вврхнюю часть груди",
-            &format!("http://{IP}/images/bench_dmb30.jpeg")
+            &format!("http://{IMAGE_IP}/images/bench_dmb30.jpeg")
         )
     );
 
@@ -213,7 +213,7 @@ async fn get_exercises() -> impl Responder {
             "chest",
             "Жим узким хватом",
             "Жим узким хватом — это базовое упражнение на прокачку грудных мышц и трицепсов, при выполнении которого руки на грифе находятся на расстоянии около 40 см друг от друга.",
-            &format!("http://{IP}/images/bench_close.jpeg")
+            &format!("http://{IMAGE_IP}/images/bench_close.jpeg")
         )
     );
 
@@ -222,7 +222,7 @@ async fn get_exercises() -> impl Responder {
             "chest",
             "Пуловер",
             "Пуловер — изолированное упражнение для проработки верхней части спины, при этом незначительно нагружаются грудные мышцы и трицепсы.",
-            &format!("http://{IP}/images/pullover.jpeg")
+            &format!("http://{IMAGE_IP}/images/pullover.jpeg")
         )
     );
 
@@ -231,7 +231,7 @@ async fn get_exercises() -> impl Responder {
             "chest",
             "Разводка в тренажёре",
             "В тренажере мышца всегда находится под нагрузкой. Тяжесть упражнения возрастает по мере сведения рук",
-            &format!("http://{IP}/images/chest_together.jpeg")
+            &format!("http://{IMAGE_IP}/images/chest_together.jpeg")
         )
     );
 
@@ -240,7 +240,7 @@ async fn get_exercises() -> impl Responder {
             "chest",
             "Разводка в кроссе",
             "Эффективное изолированное упражнение для развития мышц груди.",
-            &format!("http://{IP}/images/chest_together_cross.jpeg")
+            &format!("http://{IMAGE_IP}/images/chest_together_cross.jpeg")
         )
     );
 
@@ -249,7 +249,7 @@ async fn get_exercises() -> impl Responder {
             "chest",
             "Брусья",
             "Нагрузка приходится на трицепс и мышцы груди.",
-            &format!("http://{IP}/images/bars.png")
+            &format!("http://{IMAGE_IP}/images/bars.png")
         )
     );
 
@@ -258,7 +258,7 @@ async fn get_exercises() -> impl Responder {
             "chest",
             "Отжимания",
             "Отжима́ния — базовое физическое упражнение, выполняемое в планке и представляющее собой опускание-поднятие тела с помощью рук от пола, скамьи, стула, стола, стены и т. д.",
-            &format!("http://{IP}/images/pushup.jpeg")
+            &format!("http://{IMAGE_IP}/images/pushup.jpeg")
         )
     );
     //END CHEST
@@ -268,7 +268,7 @@ async fn get_exercises() -> impl Responder {
             "shoulders",
             "Жим стоя",
             "Базовое упражнение с классической техникой (стоя со штангой) задействует много мышечных групп, но лучше всего прокачивает передние дельты, делая плечи визуально более объемными.",
-            &format!("http://{IP}/images/shoulder_press.jpeg")
+            &format!("http://{IMAGE_IP}/images/shoulder_press.jpeg")
         )
     );
 
@@ -277,7 +277,7 @@ async fn get_exercises() -> impl Responder {
             "shoulders",
             "Жим сидя с гантелями",
             "Жим гантелей сидя – классическое упражнение для проработки передней и средней дельтовидных мышц и в меньшей степени трицепса.",
-            &format!("http://{IP}/images/seated_press.jpeg")
+            &format!("http://{IMAGE_IP}/images/seated_press.jpeg")
         )
     );
 
@@ -286,7 +286,7 @@ async fn get_exercises() -> impl Responder {
             "shoulders",
             "Круг вращения",
             "Гало — прекрасное упражнение для улучшения подвижности ваших плеч, предложенное мастером русского гиревого спорта Стивом Максвелом.",
-            &format!("http://{IP}/images/galo.jpeg")
+            &format!("http://{IMAGE_IP}/images/galo.jpeg")
         )
     );
 
@@ -295,7 +295,7 @@ async fn get_exercises() -> impl Responder {
             "shoulders",
             "Отвидения",
             "За отведение руки в стороны от туловища отвечают надостная и дельтовидная мышцы.",
-            &format!("http://{IP}/images/visions.png")
+            &format!("http://{IMAGE_IP}/images/visions.png")
         )
     );
 
@@ -304,7 +304,7 @@ async fn get_exercises() -> impl Responder {
             "shoulders",
             "Батерфляй",
             "Тяга к подбородку — базовое упражнения для тренировки дельтовидных мышц (главным образом средней головки). В упражнении также участвует трапециевидные мышцы.",
-            &format!("http://{IP}/images/pull_head.jpeg")
+            &format!("http://{IMAGE_IP}/images/pull_head.jpeg")
         )
     );
 
@@ -313,7 +313,7 @@ async fn get_exercises() -> impl Responder {
             "shoulders",
             "Россомаха",
             "Упражнение на задний пучок. Скрещивание рук, затем разводка",
-            &format!("http://{IP}/images/wolf.jpeg")
+            &format!("http://{IMAGE_IP}/images/wolf.jpeg")
         )
     );
     //END SHOULDERS
@@ -323,7 +323,7 @@ async fn get_exercises() -> impl Responder {
             "back",
             "Тяга верхнего блока",
             "Это одно из основных упражнений в фитнесе и культуризме, которое действует на внешние края широчайшей мышцы, а также на верхнюю часть грудной мышцы, в меньшей мере на дельтовидные мышцы и бицепсы рук",
-            &format!("http://{IP}/images/upper_block.jpeg")
+            &format!("http://{IMAGE_IP}/images/upper_block.jpeg")
         )
     );
 
@@ -332,7 +332,7 @@ async fn get_exercises() -> impl Responder {
             "back",
             "Тяга нижнего блока",
             "Тяга нижнего блока отлично укрепляет мышцы средней и нижней части спины, и как следствие, улучшается осанка.",
-            &format!("http://{IP}/images/down_block.jpeg")
+            &format!("http://{IMAGE_IP}/images/down_block.jpeg")
         )
     );
 
@@ -341,7 +341,7 @@ async fn get_exercises() -> impl Responder {
             "back",
             "Рычажная тяга",
             "Тяга вертикального рычажного тренажёра широким хватом качает верх широчайших мышц спины, так же включает в работу ромбовидные и трапециевидные мышцы спины.",
-            &format!("http://{IP}/images/linkage.jpeg")
+            &format!("http://{IMAGE_IP}/images/linkage.jpeg")
         )
     );
 
@@ -350,7 +350,7 @@ async fn get_exercises() -> impl Responder {
             "back",
             "Тяга в наклоне",
             "Это упражнение включает в работу среднюю и нижнюю часть трапециевидных, широчайшие и подостные мышцы, разгибатели позвоночника и задние дельты",
-            &format!("http://{IP}/images/bent_over.jpeg")
+            &format!("http://{IMAGE_IP}/images/bent_over.jpeg")
         )
     );
 
@@ -359,7 +359,7 @@ async fn get_exercises() -> impl Responder {
             "back",
             "Тяга гантелей одной рукой",
             "Основная работающая мышечная группа при тяге гантели в наклоне одной рукой – широчайшие мышцы спины.",
-            &format!("http://{IP}/images/one_arm_dumbbell.jpeg")
+            &format!("http://{IMAGE_IP}/images/one_arm_dumbbell.jpeg")
         )
     );
 
@@ -368,7 +368,7 @@ async fn get_exercises() -> impl Responder {
             "back",
             "Подтягивания",
             "Это базовое физическое упражнение, развивающее группы мышц верхней части тела: широчайшие, бицепсы, брахиалис, грудные, верхний отдел позвоночника, мышцы брюшной стенки, предплечья.",
-            &format!("http://{IP}/images/pullup.webp")
+            &format!("http://{IMAGE_IP}/images/pullup.webp")
         )
     );
 
@@ -377,7 +377,7 @@ async fn get_exercises() -> impl Responder {
             "back",
             "Гиперэкстензия",
             "Это изолированное физическое упражнение для развития выпрямителей спины.",
-            &format!("http://{IP}/images/hyperextension.webp")
+            &format!("http://{IMAGE_IP}/images/hyperextension.webp")
         )
     );
     //END BACK
@@ -387,7 +387,7 @@ async fn get_exercises() -> impl Responder {
             "arms",
             "Трицепс канатами",
             "Опуская кроссовер плавно вниз, зафиксируйте результат в самой нижней точке на пару секунд, Так как именно в нижней точке трицепс напряжен больше всего.",
-            &format!("http://{IP}/images/ropes.jpeg")
+            &format!("http://{IMAGE_IP}/images/ropes.jpeg")
         )
     );
 
@@ -396,7 +396,7 @@ async fn get_exercises() -> impl Responder {
             "arms",
             "Французкий жим",
             "Французский жим – упражнение, при котором основная нагрузка приходится на разгибающие мышцы рук – трицепсы. Также в работу включаются мышцы груди, плеч и предплечья, но в меньшей степени.",
-            &format!("http://{IP}/images/french.jpeg")
+            &format!("http://{IMAGE_IP}/images/french.jpeg")
         )
     );
 
@@ -405,7 +405,7 @@ async fn get_exercises() -> impl Responder {
             "arms",
             "Бицепс стоя",
             "Во время подъема штанги не двигайте локтями, держите их по бокам туловища и не сгибайте руки в запястьях.",
-            &format!("http://{IP}/images/biceps_class.jpeg")
+            &format!("http://{IMAGE_IP}/images/biceps_class.jpeg")
         )
     );
 
@@ -414,7 +414,7 @@ async fn get_exercises() -> impl Responder {
             "arms",
             "Бицепс молотки",
             "Мóлот — изолирующее упражнение для развития мышц боковой части двуглавой мышцы плеча, плечевой и плечелучевой мышц.",
-            &format!("http://{IP}/images/hummers.jpeg")
+            &format!("http://{IMAGE_IP}/images/hummers.jpeg")
         )
     );
 
@@ -423,7 +423,7 @@ async fn get_exercises() -> impl Responder {
             "arms",
             "Бицепс в кроссе",
             "Мóлот В этом упражнении сгибания рук на бицепс задействуют все пять компонентов бицепса.",
-            &format!("http://{IP}/images/biceps_block.jpeg")
+            &format!("http://{IMAGE_IP}/images/biceps_block.jpeg")
         )
     );
 
